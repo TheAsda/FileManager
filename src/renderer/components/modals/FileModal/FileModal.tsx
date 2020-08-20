844253294dea85c2da5f733550f69abe453ee966
Andrey Kiselev <omega-faworit@yandex.ru>
2020-08-20 22:11:42 +0300

    feat: add popup after action

--- src/renderer/components/modals/FileModal/FileModal.tsx
+++ src/renderer/components/modals/FileModal/FileModal.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState, useCallback } from 'react';
 import { InputModal } from '../InputModal';
 import { fileActionStore, FileActionStore, fileActionApi } from 'renderer/store';
 import { useDirectoryManager } from '@fm/hooks';
+import { toast } from 'react-toastify';
 
 const FileModal = () => {
   const { directoryManager } = useDirectoryManager();
@@ -19,8 +20,18 @@ const FileModal = () => {
       if (state.info) {
         if (state.info.type === 'copy') {
           directoryManager.copyItems(state.info.files, destinationPath);
+          toast(
+            `Copied ${
+              state.info.files.length === 1 ? 'file' : `${state.info.files.length} files`
+            } to ${destinationPath}`
+          );
         } else {
           directoryManager.moveItems(state.info.files, destinationPath);
+          toast(
+            `Moved ${
+              state.info.files.length === 1 ? 'file' : `${state.info.files.length} files`
+            } files to ${destinationPath}`
+          );
         }
       }
       close();
