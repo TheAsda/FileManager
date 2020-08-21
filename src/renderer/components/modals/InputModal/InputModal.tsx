afa4edd70296a8b75507398c879c4c78dba1b2cb
Andrey Kiselev <omega-faworit@yandex.ru>
2020-08-21 20:05:14 +0300

    style: input modal

--- src/renderer/components/modals/InputModal/InputModal.tsx
+++ src/renderer/components/modals/InputModal/InputModal.tsx
@@ -1,6 +1,36 @@
 import React, { useState, useEffect } from 'react';
 import Modal from 'react-modal';
-import './style.css';
+import styled from 'styled-components';
+import { Theme } from '@fm/common';
+import { useTheme } from '@fm/hooks';
+
+const Title = styled.h1<Theme>`
+  font-size: ${(props) => props['palette.fontSize'] * 1.5}px;
+`;
+
+const Button = styled.button<Theme>`
+  background-color: ${(props) => props['palette.backgroundColor']};
+  font-size: ${(props) => props['palette.fontSize']}px;
+  color: ${(props) => props['palette.textColor']};
+`;
+
+const Content = styled.div`
+  margin: 10px 0;
+`;
+
+const SubHeader = styled.h2<Theme>`
+  font-size: ${(props) => props['palette.fontSize'] * 1.2}px;
+`;
+
+const Input = styled.input<Theme>`
+  background-color: ${(props) => props['palette.additionalColor']};
+`;
+
+const Footer = styled.div`
+  display: flex;
+  flex-flow: row nowrap;
+  justify-content: space-between;
+`;
 
 interface InputModalProps {
   isOpened: boolean;
@@ -12,6 +42,7 @@ interface InputModalProps {
 }
 
 const InputModal = (props: InputModalProps) => {
+  const { theme } = useTheme();
   const [state, setState] = useState<string>(props.initialValue ?? '');
 
   const accept = () => {
@@ -30,16 +61,37 @@ const InputModal = (props: InputModalProps) => {
       isOpen={props.isOpened}
       onRequestClose={props.onClose}
       shouldCloseOnOverlayClick={true}
+      style={{
+        content: {
+          padding: '10px',
+          color: theme['palette.textColor'],
+          backgroundColor: theme['palette.backgroundColor'],
+          top: '50%',
+          left: '50%',
+          bottom: undefined,
+          right: undefined,
+          transform: 'translate(-50%,-50%)',
+        },
+      }}
     >
-      <h1>{props.title}</h1>
-      <div>
-        <h2>{props.subtitle}</h2>
-        <input onChange={(event) => setState(event.target.value)} value={state} />
-      </div>
-      <div>
-        <button onClick={props.onClose}>Cancel</button>
-        <button onClick={accept}>Ok</button>
-      </div>
+      <Title {...theme}>{props.title}</Title>
+      <Content>
+        <SubHeader {...theme}>{props.subtitle}</SubHeader>
+        <Input
+          {...theme}
+          onChange={(event) => setState(event.target.value)}
+          value={state}
+          autoFocus
+        />
+      </Content>
+      <Footer>
+        <Button {...theme} onClick={props.onClose}>
+          Cancel
+        </Button>
+        <Button {...theme} onClick={accept}>
+          Ok
+        </Button>
+      </Footer>
     </Modal>
   );
 };
