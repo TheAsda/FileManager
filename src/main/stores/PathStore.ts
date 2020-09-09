600586e661df8f23c7df7db275d651f56d5c3ffa
Asda <omega-faworit@yandex.ru>
2020-09-09 11:27:14 +0300

    feat(main): place paths array into object

--- src/main/stores/PathStore.ts
+++ src/main/stores/PathStore.ts
@@ -1,21 +1,28 @@
 import { IPathStore } from '../interfaces/IPathStore';
 import Store from 'electron-store';
+import { debug } from 'electron-log';
 
 class PathStore implements IPathStore {
-  store: Store<string[]>;
+  store: Store<{ list: string[] }>;
 
   constructor() {
-    this.store = new Store({
+    this.store = new Store<{ list: string[] }>({
       name: 'paths',
+      defaults: {
+        list: [],
+      },
     });
   }
 
   getPaths(): string[] {
-    return this.store.store;
+    return this.store.store.list;
   }
 
   addToPath(path: string): void {
-    this.store.set([path]);
+    debug(`Adding path ${path}`);
+    this.store.set({
+      list: [...this.store.store.list, path],
+    });
   }
 }
 
