246680d60031d00a8de0ca7cb47c8b70960200a4
Asda <omega-faworit@yandex.ru>
2020-09-09 11:21:56 +0300

    feat(main): add ipc logging

--- src/main/ipc.ts
+++ src/main/ipc.ts
@@ -1,7 +1,12 @@
 import { ipcMain, IpcMainEvent } from 'electron';
+import { debug } from 'electron-log';
 
-const registerIpc = <T>(channer: string, handler: (event: IpcMainEvent, message: T) => void) => {
-  ipcMain.on(channer, handler);
+const registerIpc = <T>(channel: string, handler: (event: IpcMainEvent, message: T) => void) => {
+  const handlerWithLog = (event: IpcMainEvent, message: T) => {
+    debug(`Got message: ${JSON.stringify(message)} on channel ${channel}`);
+    handler(event, message);
+  };
+  ipcMain.on(channel, handlerWithLog);
 };
 
 const unregisterIpc = (channel: string) => {
